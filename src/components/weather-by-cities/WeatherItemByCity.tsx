09e21f64e394ba7c85c13afe71bb72cf3e54e781
Tamas Marton <hello@tamasmarton.com>
2023-02-13 14:13:56 +0100

    Query cities in a loop with useQueries()

--- src/components/weather-by-cities/WeatherItemByCity.tsx
+++ src/components/weather-by-cities/WeatherItemByCity.tsx
@@ -1,27 +1,23 @@
-import { useQuery } from 'react-query'
-
 import { useUnit } from '~/context/unitContext'
-import { getCurrentWeatherByCity } from '~/Fetchers'
 import { formatTemp } from '~/utils/formatTemp'
 
-const WeatherItemByCity = ({ city }: { city: string }) => {
+const WeatherItemByCity = ({ result }: { result: any }) => {
   const unit = useUnit()
 
-  const { data } = useQuery(`currentWeatherIn${city}`, getCurrentWeatherByCity(city))
-
-  if (!data) return <p>An error occurred.</p>
+  if (result.isLoading) return <p>Loading...</p>
+  if (result.error) return <p>An error occurred.</p>
 
   return (
     <div className='grid grid-cols-3 items-center'>
-      <span className='text-left'>{data.name}</span>
+      <span className='text-left'>{result?.data.name}</span>
 
       <img
-        alt={data?.weather[0].icon}
-        src={`http://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}
+        alt={result?.data?.weather[0].icon}
+        src={`http://openweathermap.org/img/wn/${result?.data?.weather[0].icon}@2x.png`}
         className='w-10 h-10 my-2 mx-auto'
       />
 
-      <span className='text-right'>{formatTemp(data?.main.temp, unit)}</span>
+      <span className='text-right'>{formatTemp(result?.data?.main.temp, unit)}</span>
     </div>
   )
 }
--- src/components/weather-by-cities/index.tsx
+++ src/components/weather-by-cities/index.tsx
@@ -1,12 +1,24 @@
+import { useQueries } from 'react-query'
+
 import WeatherItemByCity from '~/components/weather-by-cities/WeatherItemByCity'
+import { getCurrentWeatherByCity } from '~/Fetchers'
 
 const WeatherByCities = ({ cityList }: { cityList: string[] }) => {
+  const cityResults = useQueries(
+    cityList.map(city => {
+      return {
+        queryKey: `currentWeatherIn${city}`,
+        queryFn: getCurrentWeatherByCity(city),
+      }
+    })
+  )
+
   return (
     <div className='w-full sm:w-3/4 md:w-2/3 px-16 sm:px-8 md:px-0 mt-8 mx-auto'>
-      {cityList.map((city, idx) => (
+      {cityResults.map((cityResult, idx) => (
         <WeatherItemByCity
           key={idx}
-          city={city}
+          result={cityResult}
         />
       ))}
     </div>
